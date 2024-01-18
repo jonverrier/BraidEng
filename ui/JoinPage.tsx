57e21e3c5c5abf9c716f115ae8ab4ad1f7c26854
Jon <jonathan.verrier@hotmail.com>
2024-01-18 22:33:33 +0000

    Start of Join Page.

--- ui/JoinPage.tsx
+++ ui/JoinPage.tsx
@@ -6,12 +6,13 @@ import React, { ChangeEvent, useState } from 'react';
 // Fluent
 import {
    makeStyles, Button, Tooltip,
-   useId, Input, 
+   Text, Input, 
    InputOnChangeData
 } from '@fluentui/react-components';
 
 import {
-   Person24Regular
+   Person24Regular,
+   Key24Regular
 } from '@fluentui/react-icons';
 
 import { EStrings } from './UIStrings';
@@ -21,33 +22,62 @@ export interface IJoinPageProps {
 
 const viewOuterStyles = makeStyles({
    root: {
+      display: 'flex',
+      flexDirection: 'row',
       height: '100vh', /* fill the screen with flex layout */ 
       paddingLeft: '5px',
       paddingRight: '5px',
       paddingTop: '5px',
-      paddingBottom: '5px'
+      paddingBottom: '5px',
+      textAlign: 'center',
+      alignItems: 'top'      
+   },
+});
+
+const leftColumnStyles = makeStyles({
+   root: {
+      display: 'flex',
+      flexDirection: 'column',
+      width: '10%',
+      alignSelf: 'left',
+      marginLeft: '0',
+      marginRight: 'auto'
+   },
+});
+
+const rightColumnStyles = makeStyles({
+   root: {
+      display: 'flex',
+      flexDirection: 'column',
+      width: '10%',
+      alignSelf: 'right',
+      marginLeft: 'auto',
+      marginRight: '0'
    },
 });
 
 const centerColumnStyles = makeStyles({
-   root: {
+   root: {    
       display: 'flex',
-      flexDirection: 'row',
-      alignSelf: 'center',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      alignItems: 'center'
+      flexDirection: 'column',
+      marginLeft: '0px',
+      marginRight: '0px',
+      textAlign: 'center'
    },
 });
 
 export const JoinPage = (props: IJoinPageProps) => {
 
    const viewOuterClasses = viewOuterStyles();
-   const centerControlClasses = centerColumnStyles();
+   const leftColumnClasses = leftColumnStyles();
+   const centerColumnClasses = centerColumnStyles();
+   const rightColumnClasses = rightColumnStyles();
+
 
    const nameInputId = "nameInputId";
    
    const [name, setName] = useState<string>("");
+   const [key, setKey] = useState<string>("");
 
    function onJoinAsChange(ev: ChangeEvent<HTMLInputElement>, data: InputOnChangeData): void {
 
@@ -55,22 +85,44 @@ export const JoinPage = (props: IJoinPageProps) => {
       // joinPrompt = joinPromptFromName(name, joinPromptEnabled, joinPromptDisabled);
    }
 
+   function onKeyChange(ev: ChangeEvent<HTMLInputElement>, data: InputOnChangeData): void {
+
+      setKey(data.value);
+      // joinPrompt = joinPromptFromName(name, joinPromptEnabled, joinPromptDisabled);
+   }   
+
    return (
-      <div className={viewOuterClasses.root} >        
-         <div className={centerControlClasses.root}>
+      <div className={viewOuterClasses.root} >     
+         <div className={leftColumnClasses.root}></div>         
+         <div className={centerColumnClasses.root}>
+            <Text align="justify">{EStrings.kJoinPagePreamble}</Text>
+            &nbsp;               
             <Tooltip withArrow content={EStrings.kJoinConversationAsPrompt} relationship="label">
                   <Input id={nameInputId} aria-label={EStrings.kJoinConversationAsPrompt}
+                     required={true}
                      value={name}
+                     maxLength={20}
                      contentBefore={<Person24Regular />}
                      placeholder={EStrings.kJoinConversationAsPlaceholder}
                      onChange={onJoinAsChange}
                      disabled={false}
                   />
-            </Tooltip>           
-            <p>
-               Join Page
-            </p> 
+            </Tooltip>      
+            &nbsp;   
+            <Tooltip withArrow content={EStrings.kJoinConversationKeyPrompt} relationship="label">
+                  <Input id={nameInputId} aria-label={EStrings.kJoinConversationKeyPrompt}
+                     required={true}                  
+                     value={name}
+                     maxLength={40}
+                     contentBefore={<Key24Regular />}
+                     placeholder={EStrings.kJoinConversationKeyPlaceholder}
+                     onChange={onKeyChange}
+                     disabled={false}
+                  />
+            </Tooltip>             
+            &nbsp;                  
          </div>
+         <div className={rightColumnClasses.root}></div>           
       </div>
       );
 }
--- ui/UIStrings.ts
+++ ui/UIStrings.ts
@@ -2,7 +2,10 @@
 
 export enum EStrings {
 
-   kJoinConversationAsPrompt = "Join as (name/initials):",
-   kJoinConversationAsPlaceholder = "Join as... "
+   kJoinPagePreamble = "To join a conversation with the Braid FSBot, you need to enter your name or initials to identify yourself in the conversation, and you need to enter the key from your contact at Braid.",
+   kJoinConversationAsPrompt = "Join as (name/initials)",
+   kJoinConversationAsPlaceholder = "Join as... ",
+   kJoinConversationKeyPrompt = "Key",
+   kJoinConversationKeyPlaceholder = "Key... "   
    
 };
