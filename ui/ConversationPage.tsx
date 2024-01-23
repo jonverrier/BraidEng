9d4daa0c0f7c246af69e1a90872d6f129f813b1c
Jon <jonathan.verrier@hotmail.com>
2024-01-23 10:08:33 +0000

    Start conversation page

--- ui/ConversationPage.tsx
+++ ui/ConversationPage.tsx
@@ -5,18 +5,29 @@ import React, { ChangeEvent, useState } from 'react';
 
 // Fluent
 import {
-   makeStyles, Button, Tooltip,
+   makeStyles, shorthands, Button, Tooltip,
+   Body1,
+   Caption1,
    Text, Input, 
-   InputOnChangeData
+   InputOnChangeData,
+   Card,
+   CardFooter,
+   CardHeader,
+   CardPreview  
 } from '@fluentui/react-components';
 
 import {
    Person24Regular,
-   Key24Regular
+   Key24Regular,
+   ArrowReplyRegular
 } from '@fluentui/react-icons';
 
 import { EUIStrings } from './UIStrings';
 import { EConfigStrings } from './ConfigStrings';
+import { EIcon } from '../core/Icons';
+import { UuidKeyGenerator } from '../core/UuidKeyGenerator';
+import { Persona } from '../core//Persona';
+import { Message } from '../core/Message';
 
 export interface IConversationPageProps {
 
@@ -41,9 +52,68 @@ export const ConversationPage = (props: IConversationPageProps) => {
 
    const viewOuterClasses = viewOuterStyles();
 
+   let keyGenerator = new UuidKeyGenerator();
+   let person = new Persona (keyGenerator.generateKey(), "Jon", EIcon.kPersonPersona, undefined, new Date());
+   let bot = new Persona (keyGenerator.generateKey(), "FSChatBot", EIcon.kBotPersona, undefined, new Date());  
+   
+   let personMessage = new Message (keyGenerator.generateKey(), person.id, undefined, "Hello, from a person.", new Date());
+   let botMessage = new Message (keyGenerator.generateKey(), bot.id, undefined, "Hello, from a bot.", new Date());
+
+   let messages = new Array<Message>();
+   messages.push (personMessage);
+   messages.push (botMessage);   
+
    return (
       <div className={viewOuterClasses.root} >     
-         
+         <MessageView message={messages[0]} author={person}></MessageView>
+         <MessageView message={messages[1]} author={bot}></MessageView>         
       </div>
       );
 }
+
+export interface IMessageViewProps {
+
+   message: Message;  
+   author: Persona;
+}
+
+const messageViewStyles = makeStyles({
+   card: {
+     ...shorthands.margin("auto"),
+     width: "720px",
+     maxWidth: "100%",
+   },
+ });
+
+export const MessageView = (props: IMessageViewProps) => {
+
+   const styles = messageViewStyles();
+ 
+   return (
+     <Card className={styles.card}>
+       <CardHeader
+         image={
+            <Person24Regular/>
+         }
+         header={
+           <Body1>
+             <b>author.name</b> 
+           </Body1>
+         }
+         description={<Caption1>props.message.text</Caption1>}
+       />
+ 
+       <CardPreview
+         logo={
+            <Person24Regular/>
+         }
+       >
+       </CardPreview>
+ 
+       <CardFooter>
+         <Button icon={<ArrowReplyRegular/>}>Reply</Button>
+       </CardFooter>
+     </Card>
+   );
+}
+
