7a2ad0cd82608c30367910df4f0985826ffae040
Jon <jonathan.verrier@hotmail.com>
2024-02-07 21:05:06 +0000

    API name changes

--- core/ConfigStrings.ts
+++ core/ConfigStrings.ts
@@ -4,8 +4,8 @@ export enum EConfigStrings {
 
    kApiLogCategory = "API",
 
-   kRequestJoinKeyUrl = "https://ambitious-ground-0a343ae03.4.azurestaticapps.net/api/key",
-   kRequestConversationKeyUrl = "https://ambitious-ground-0a343ae03.4.azurestaticapps.net/api/conversation",   
+   kRequestJoinKeyUrl = "https://ambitious-ground-0a343ae03.4.azurestaticapps.net/api/joinkey",
+   kRequestAiKeyUrl = "https://ambitious-ground-0a343ae03.4.azurestaticapps.net/api/aikey",   
    kRequestKeyParameterName = "JoinKey",
 
    kAzureTenantId = "5dcb73bd-782d-4979-9c12-4f14106313b0",
@@ -16,8 +16,8 @@ export enum EConfigStrings {
    kBotRequestSignature = '@BraidBot',
    kOpenAiPersonaPrompt = "You are a customer service agent for a bank, skilled in explaining complex products in simple language. You limit replies to 100 words or less.",
 
-   kErrorConnectingToKeyAPI = "Error connecting to server.",
-   kErrorConnectingToAiAPI = "Error connecting to AI." 
+   kErrorConnectingToKeyAPI = "Error connecting to Braid server.",
+   kErrorConnectingToAiAPI = "Error connecting to AI server." 
 };
 
 // This is ised for local running only, as in browser we cannot access environment variables
